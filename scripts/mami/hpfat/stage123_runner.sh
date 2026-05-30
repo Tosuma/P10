a5cb4aa11f46403ce45de9211adba91ff53c278d
faering666 <huginj@gmail.com>
2026-05-30 09:15:51 +0200

    Correct arrengement

--- scripts/mami/hpfat/stage123_runner.sh
+++ scripts/mami/hpfat/stage123_runner.sh
@@ -7,8 +7,8 @@ STAGE23_JOB_SCRIPT="${REPO_ROOT}/scripts/mami/hpfat/stage2_stage3_job.sh"
 
 STAGE1_LR="4e-4"
 STAGE1_MRAE="1.0"
-STAGE1_NDVI="0.5"
 STAGE1_NDRE="0.1"
+STAGE1_NDVI="0.5"
 STAGE1_DIR="hpfat/full/stage1"
 STAGE1_MODEL="hpfat-andhra-re_0.1-vi_0.5"
 
